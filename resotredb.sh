829da3adbd13cb87248aeaea2b2d3ef1bb33c652
Md. Masud Parvaze <141165442+parvaze-masud@users.noreply.github.com>
2023-10-23 23:14:05 +0600

    Update resotredb.sh

--- resotredb.sh
+++ resotredb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /root/bhw/ 
-cat SQL\ File/bhw.sql | docker exec -i bhw_backend_1 /usr/bin/mysql -u root --password=Time@12345 bhw
+cat dbdump/bhw.sql | docker exec -i bhw_backend_1 /usr/bin/mysql -u root --password=Time@12345 bhw
